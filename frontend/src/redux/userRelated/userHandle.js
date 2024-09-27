768858d53286182850615b82c5cb4f15469949a0
Nikhil18Patil <nikhilpatil18012004@gmail.com>
2024-09-28 00:50:47 +0530

    backend code update

--- frontend/src/redux/userRelated/userHandle.js
+++ frontend/src/redux/userRelated/userHandle.js
@@ -14,7 +14,7 @@ import {
 } from './userSlice';
 import { Navigate } from 'react-router-dom';
 
-const REACT_APP_BASE_URL = "https://school-mangement-crm-guld-kbyv0rmru-nikhil18patils-projects.vercel.app/";
+const REACT_APP_BASE_URL = "https://school-mangement-crm-guld.vercel.app";
 
 export const loginUser = (fields, role) => async (dispatch) => {
     dispatch(authRequest());
