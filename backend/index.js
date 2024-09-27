298b00192c7eafea1f235f7727e7922c4539a678
Nikhil18Patil <nikhilpatil18012004@gmail.com>
2024-09-28 01:56:08 +0530

    Db update

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors({
   }));
 
 mongoose
-    .connect(process.env.DB, {
+    .connect('mongodb+srv://adityashivhare7291:jq1QfSZQvyEaPxCl@cluster0.cvliz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
